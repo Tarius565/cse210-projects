c6d1945aa65ec4fbf01b55d34289f82bb5bec21e
Tarius565 <tlevi999@gmail.com>
2023-02-27 06:06:26 -0700

    shapes

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        // Square s1 = new Square("Purple", 5);
+
+        // Console.WriteLine($"Color {s1.GetColor()}. Area {s1.GetArea()}");
+
+
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Purple", 5);
+        shapes.Add(s1);
+
+        Rectangle s2 = new Rectangle("Green", 5, 2);
+        shapes.Add(s2);
+
+        Circle s3 = new Circle("Red", 4);
+        shapes.Add(s3);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+
+            double area = shape.GetArea();
+            
+            Console.WriteLine($"Color: {color}. Area: {area}");
+        }
+
     }
 }
